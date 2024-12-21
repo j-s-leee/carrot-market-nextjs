4aeece6615e7e84cdc2c5b9633d875775c021616
j-s-leee <q216044@gmail.com>
2024-12-21 18:18:15 +0900

    feat: log in with email.

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,9 +1,26 @@
-"use server"
+"use server";
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
-import {z} from "zod"
+import db from "@/lib/db";
+import {z} from "zod";
+import bcrypt from "bcrypt";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const checkEmailExists = async (email:string) => {
+    const user = await db.user.findUnique({
+        where: {
+            email
+        },
+        select: {
+            id: true
+        }
+    });
+    return !Boolean(user);
+}
 
 const formSchema = z.object({
-    email: z.string().email().toLowerCase(),
+    email: z.string().email().toLowerCase()
+    .refine(checkEmailExists, "An account with this email does not exist."),
     password: z.string({
         required_error: "Password is required"
     })
@@ -16,11 +33,32 @@ export async function login(prevState: any, formData: FormData) {
             email: formData.get('email'),
             password: formData.get('password')
         };
-        const result = formSchema.safeParse(data);
+        const result = await formSchema.spa(data);
         if (!result.success) {
             return result.error.flatten();
         } else {
-            console.log(result.data);
+            const user = await db.user.findUnique({
+                where: {
+                    email: result.data.email
+                },
+                select: {
+                    id: true,
+                    password: true,
+                }
+            });
+            const ok = await bcrypt.compare(result.data.password, user!.password ?? "xxxx");
+            if (ok) {
+                const session = await getSession();
+                session.id = user?.id;
+                redirect("/profile");
+            } else {
+                return {
+                    fieldErrors: {
+                        email: [],
+                        password: ["Wrong password."],
+                    }
+                }
+            }
         }
 
     }
