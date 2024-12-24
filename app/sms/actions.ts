010430958834fc54b75db03c85cec87955ffb1bf
j-s-leee <q216044@gmail.com>
2024-12-25 01:02:45 +0900

    feat: create and save sms token

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,6 +2,8 @@
 import {z} from "zod"
 import validator from "validator"
 import { redirect } from "next/navigation";
+import crypto from "crypto";
+import db from "@/lib/db";
 
 const phoneSchema = z.string()
     .trim()
@@ -10,6 +12,10 @@ const phoneSchema = z.string()
         "Wrong phone format"
     );
 const tokenSchema = z.coerce.number().min(100000).max(999999);
+
+async function getToken() {
+    return crypto.randomInt(100000, 999999).toString();
+}
 
 interface ActionState {
     token: boolean;
@@ -26,6 +32,39 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
                 error: result.error.flatten(),
             }
         } else {
+            // trunk token with phone number
+            await db.sMSToken.deleteMany({
+                where: {
+                    user: {
+                        phone: result.data
+                    }
+                }
+            });
+
+            // create token
+            const token = await getToken();
+            
+            // save token
+            db.sMSToken.create({
+                data: {
+                    token,
+                    user: {
+                        connectOrCreate: {
+                            where: {
+                                phone: result.data
+                            },
+                            create: {
+                                username: crypto.randomBytes(10).toString("hex"),
+                                phone: result.data
+                            }
+                        }
+                    }
+                }
+            })
+
+            // send token sms
+
+            // and  return
             return {
                 token: true
             }
@@ -38,6 +77,9 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
                 error: result.error.flatten(),
             }
         } else {
+            // validate token
+            // validate phone number
+            // login
             redirect("/");
         }
     }
