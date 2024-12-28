67737dad2ddca31bd8bf08c0f13c9b147801147d
j-s-leee <q216044@gmail.com>
2024-12-29 00:19:16 +0900

    design: skeletons for /products

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,3 +1,8 @@
-export default function Products() {
+async function getProducts() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+}
+
+export default async function Products() {
+  const products = await getProducts();
   return <h1>products!</h1>;
 }
