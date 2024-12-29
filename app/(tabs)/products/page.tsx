1059c985fe9f4b9c6fd657d292b2728ea5d8327c
j-s-leee <q216044@gmail.com>
2024-12-29 10:35:49 +0900

    feat: product list page

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,8 +1,26 @@
+import ListProduct from "@/components/list-product";
+import db from "@/lib/db";
+
 async function getProducts() {
-  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const products = await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
+  return products;
 }
 
 export default async function Products() {
   const products = await getProducts();
-  return <h1>products!</h1>;
+  return (
+    <div>
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
+    </div>
+  );
 }
