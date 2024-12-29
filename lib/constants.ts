c165bd8b8afb1d0a0fe7eb08a005bfde895bc274
j-s-leee <q216044@gmail.com>
2024-12-29 13:08:16 +0900

    feat: infinite scrolling

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,4 +4,4 @@ export const PASSWORD_REGEX = new RegExp(
 export const PASSWORD_REGEX_ERROR =
   "password must contain at least one lowercase, UPPERCASE, number, special characters like !@#$%^&*";
 export const PASSWORD_MIN_LENGTH = 4;
-export const PAGE_SIZE = 1;
+export const PAGE_SIZE = 2;
