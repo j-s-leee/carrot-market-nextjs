7130daa0132043ba4e989d045f59d0a20035266f
j-s-leee <q216044@gmail.com>
2024-12-29 18:55:13 +0900

    feat: upload product image to cloudflare images

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,11 +1,70 @@
 "use server";
+import z from "zod";
+import fs from "fs/promises";
+import getSession from "@/lib/session";
+import db from "@/lib/db";
+import { redirect } from "next/navigation";
 
-export async function uploadProduct(formData: FormData) {
+const productSchema = z.object({
+  photo: z.string({
+    required_error: "Photo is required",
+  }),
+  title: z.string({
+    required_error: "Title is required",
+  }),
+  description: z.string({
+    required_error: "Description is required",
+  }),
+  price: z.coerce.number({
+    required_error: "Price is required",
+  }),
+});
+
+export async function getUploadUrl() {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v2/direct_upload`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.CLOUDFLARE_TOKEN}`,
+      },
+    }
+  );
+  const data = await response.json();
+  return data;
+}
+
+export async function uploadProduct(_: any, formData: FormData) {
   const data = {
     photo: formData.get("photo"),
     title: formData.get("title"),
     price: formData.get("price"),
     description: formData.get("description"),
   };
-  console.log(data);
+
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          photo: result.data.photo,
+          price: result.data.price,
+          description: result.data.description,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      redirect(`/products/${product.id}`);
+    }
+  }
 }
--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -3,12 +3,17 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
-import { uploadProduct } from "./actions";
+import { useActionState, useState } from "react";
+import { getUploadUrl, uploadProduct } from "./actions";
+import { env } from "process";
+import { CLOUDFLARE_DELIVERY_URL } from "@/lib/constants";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
-  const onImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+
+  const [uploadUrl, setUploadUrl] = useState("");
+  const [photoId, setPhotoId] = useState("");
+  const onImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { files },
     } = e;
@@ -17,10 +22,36 @@ export default function AddProduct() {
     const file = files[0];
     const url = URL.createObjectURL(file);
     setPreview(url);
+    const { success, result } = await getUploadUrl();
+    if (success) {
+      const { id, uploadURL } = await result;
+      setUploadUrl(uploadURL);
+      setPhotoId(id);
+    }
   };
+
+  const interceptAction = async (_: any, formData: FormData) => {
+    const file = formData.get("photo");
+    if (!file) return;
+
+    const cloudflareForm = new FormData();
+    cloudflareForm.append("file", file);
+    const response = await fetch(uploadUrl, {
+      method: "POST",
+      body: cloudflareForm,
+    });
+
+    if (response.status !== 200) return;
+
+    const photoUrl = `${CLOUDFLARE_DELIVERY_URL}${photoId}`;
+    formData.set("photo", photoUrl);
+
+    return uploadProduct(_, formData);
+  };
+  const [state, dispatch] = useActionState(interceptAction, null);
   return (
     <div>
-      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
+      <form action={dispatch} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
           style={{ backgroundImage: `url(${preview})` }}
@@ -33,6 +64,7 @@ export default function AddProduct() {
               <PhotoIcon className="w-20" />
               <div className="text-neutral-400 text-sm">
                 사진을 추가해주세요.
+                {state?.fieldErrors.photo}
               </div>
             </>
           )}
@@ -45,9 +77,27 @@ export default function AddProduct() {
           accept="image/*"
           className="hidden"
         />
-        <Input name="title" required placeholder="제목" type="text" />
-        <Input name="price" type="number" required placeholder="가격" />
-        <Input name="description" type="text" required placeholder="설명" />
+        <Input
+          name="title"
+          required
+          placeholder="제목"
+          type="text"
+          errors={state?.fieldErrors.title}
+        />
+        <Input
+          name="price"
+          type="number"
+          required
+          placeholder="가격"
+          errors={state?.fieldErrors.price}
+        />
+        <Input
+          name="description"
+          type="text"
+          required
+          placeholder="설명"
+          errors={state?.fieldErrors.description}
+        />
         <Button text="작성 완료" />
       </form>
     </div>
