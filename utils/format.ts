92e5f1aeada01a0b78cdfdf9bdb588dafcff5b2f
j-s-leee <q216044@gmail.com>
2025-01-04 17:53:53 +0900

    feat: improve time formatting utility to handle minutes and hours

--- utils/format.ts
+++ utils/format.ts
@@ -1,12 +1,21 @@
 export function formatToTimeAgo(date: string): string {
+  const formatter = new Intl.RelativeTimeFormat("ko");
   const dayInMs = 1000 * 60 * 60 * 24;
+  const hoursInMs = 1000 * 60 * 60;
+  const minInMs = 1000 * 60;
   const time = new Date(date).getTime();
   const now = new Date().getTime();
-  const diff = Math.round((time - now) / dayInMs);
-
-  const formatter = new Intl.RelativeTimeFormat("ko");
-
-  return formatter.format(diff, "days");
+  const diff = time - now;
+  const dayDiff = Math.round(diff / dayInMs);
+  const hourDiff = Math.round(diff / hoursInMs);
+  const minDiff = Math.round(diff / minInMs);
+  if (hourDiff === 0) {
+    return formatter.format(minDiff, "minutes");
+  }
+  if (dayDiff === 0) {
+    return formatter.format(hourDiff, "hours");
+  }
+  return formatter.format(dayDiff, "days");
 }
 
 export function formatToWon(price: number): string {
